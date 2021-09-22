4db9aca6af6d4529f219038b54de60672cfde3de
Harry Shah <harvinder.shah@gmail.com>
2021-09-22 18:06:51 -0400

    front end javascript added

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,6 +3,7 @@ const router = require('express').Router();
 const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
 
+//  get all posts
 router.get('/', (req, res) => {
     Post.findAll({
         attributes: [
@@ -27,19 +28,56 @@ router.get('/', (req, res) => {
         ]
     })
     .then(dbPostData => {
-        console.log(dbPostData[0]);
-        // serialize the data by adding {plain: true}
+// serialize the data by adding {plain: true}
         const posts = dbPostData.map(post => post.get({ plain: true }));
-        res.render('homepage', { posts });
+        res.render('homepage', { posts, loggedIn: req.session.loggedIn });
     })
     .catch(err => {
         res.status(404).json(err);
     });
 });
 
-module.exports = router;
+// render single post
+router.get('/post/:id', (req, res) => {
+    Post.findOne({
+        where: {
+            id:req.params.id
+        },
+        attributes: [
+            'id',
+            'title',
+            'post_text',
+            'created_at'
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+            model: User,
+            attributes: ['username']
+            }
+        ] 
+    })
+    .then(dbPostData => {
+        if(!dbPostData){
+            res.status(404).json({ message: 'No post found with this id'});
+            return;
+        }
+        const post = dbPostData.get({ plain: true });
+        res.render('single-post', { post, loggedIn: req.session.loggedIn });
+    })
+    .catch(err => {
+        res.status(500).json(err);
+    });
+  });
 
-// login page
+  // login page
 router.get('/login', (req, res) => {
     if(req.session.loggedIn){
     res.redirect('/');
@@ -48,19 +86,4 @@ router.get('/login', (req, res) => {
     res.render('login');
 });
 
-// render single post
-router.get('/post/:id', (req, res) => {
-    const post = {
-      id: 1,
-      post_url: 'https://handlebarsjs.com/guide/',
-      title: 'Handlebars Docs',
-      created_at: new Date(),
-      vote_count: 10,
-      comments: [{}, {}],
-      user: {
-        username: 'test_user'
-      }
-    };
-  
-    res.render('single-post', { post });
-  });
+module.exports = router;
